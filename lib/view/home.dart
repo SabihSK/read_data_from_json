f9bd63a0b27bb009f201befabf7e200417fee81f
Sheikh Muhammad Sabih <sabih.sk1@gmail.com>
2021-02-14 21:03:46 +0500

    1st commit

---
+++ lib/view/home.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List data;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Load data from Json"),
+        ),
+        body: Container(
+          child: FutureBuilder(
+            future: DefaultAssetBundle.of(context)
+                .loadString("loadjson/person.json"),
+            // initialData: InitialData,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              var mydata = JsonDecoder().convert(snapshot.data.toString());
+              return ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text("Name: " + mydata[index]["name"]),
+                        Text("Age: " + mydata[index]["age"]),
+                        Text("Height: " + mydata[index]["height"]),
+                        Text("Gender: " + mydata[index]["gender"]),
+                        Text("Hair Color: " + mydata[index]["hair_color"]),
+                      ],
+                    ),
+                  );
+                },
+                itemCount: mydata == null ? 0 : mydata.length,
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
